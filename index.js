770fb3425d980916b6ee81d1b8e8c79e37c33a93
Lucas Le Roux <lerouxlrx@gmail.com>
2023-09-05 23:19:11 -0300

    Entrega 1 JS, V2 con sorteo.

--- index.js
+++ index.js
@@ -1,40 +1,93 @@
 
 alert ('¡Bienvenido a Tu Gestión! Registra tus actividades, anota tus tareas, programa tus recordatorios y conocé en detalle Tu Gestión');
 
-const nombre = prompt ('Ingresa tu nombre para avanzar: ');
-
 const imprimirLog = (dato) => console.log(dato);
+let chances = 1;
+let pregRespondidas = 0;
+let numGanador = Math.floor(Math.random() * 10) + 1;
+let ganador = false;
+function sumatoriaChances (pregunta) {
+    if (pregunta != 0) {
+        chances++;
+        pregRespondidas++;
+    }
+}
 
+
+const nombre = prompt ('Ingresa tu nombre y participa por un premio: ');
 imprimirLog ('Nombre: '+nombre);
 
-const participar = prompt ('Gracias '+nombre+'. Contesta unas preguntas y gana beneficios Responde SI, si queres participar: ');
 
-if (participar.toLowerCase() == 'si') {
+let email;
+    do {
+        email = prompt ('Necesitamos tu correo, ganas y ¡recibis el premio al instante! ');
+    } while (email == '' );
+imprimirLog('E-mail: '+email);
+
+const sumaChances = prompt ('Gracias por participar, enviaremos los resultados a '+email+'. Tenes 1 chance, contesta unas preguntas y suma más! Responde "SI", si queres aumentar tus chances: ');
+
+if (sumaChances.toLowerCase() == 'si') {
     alert ('Responde del 1 al 5 siendo 1 Nada Importante y 5 Muy Importante. Empecemos!');
-    imprimirLog ('Participa: '+participar);
+    imprimirLog ('Suma Chances: '+sumaChances);
 
     let pregImportancia;
     do {
-        pregImportancia = prompt ('Para vos, que tan importante es registrar toda Tu Gestión? Recorda responder del 1 al 5.');
-    } while (!(pregImportancia >=1 && pregImportancia <=5))
+        pregImportancia = prompt ('Para vos, que tan importante es registrar toda Tu Gestión? Recorda responder del 1 al 5. Pone 0 si queres saltear la pregunta');
+    } while (!(pregImportancia >=0 && pregImportancia <=5));
+    sumatoriaChances (pregImportancia);
+
     let pregOrden;
     do {
-        pregOrden = prompt ('Para los registros de Tu Gestión, que tan importante consideras el orden? Recorda responder del 1 al 5.');
-    } while (!(pregOrden >=1 && pregOrden <=5))
+        pregOrden = prompt ('Para los registros de Tu Gestión, que tan importante consideras el orden? Recorda responder del 1 al 5. Pone 0 si queres saltear la pregunta.');
+    } while (!(pregOrden >=0 && pregOrden <=5));
+    sumatoriaChances (pregOrden);
+
     let pregNotificaciones;
     do {
-        pregNotificaciones = prompt ('Que tan importante es tener notificaciones para que no se pase nada de Tu Gestión? Recorda responder del 1 al 5.');
-    } while (!(pregImportancia >=1 && pregNotificaciones <=5))
+        pregNotificaciones = prompt ('Que tan importante es tener notificaciones para que no se pase nada de Tu Gestión? Recorda responder del 1 al 5. Pone 0 si queres saltear la pregunta.');
+    } while (!(pregNotificaciones >=0 && pregNotificaciones <=5));
+    sumatoriaChances (pregNotificaciones);
+
     let pregReporte;
     do {
-        pregReporte = prompt ('Consideras importante tener reportería que te describa en detalle Tu Gestión? Recorda responder del 1 al 5.');
-    } while (!(pregReporte >=1 && pregReporte <=5))
+        pregReporte = prompt ('Consideras importante tener reportería que te describa en detalle Tu Gestión? Recorda responder del 1 al 5. Pone 0 si queres saltear la pregunta.');
+    } while (!(pregReporte >=0 && pregReporte <=5));
+    sumatoriaChances (pregReporte);
+
     imprimirLog ('Importancia: '+pregImportancia);
     imprimirLog ('Orden: '+pregOrden);
     imprimirLog ('Notificaciones: '+pregNotificaciones);
     imprimirLog ('Reporteria: '+pregReporte);
+    alert('Respondiste '+pregRespondidas+' preguntas, por lo que quedaste con '+chances+' chances. ¡Vamos al sorteo!');
 } else {
-    imprimirLog ('Participa: No')
+    imprimirLog ('Suma Chances: No');
+    alert('No respondiste ninguna pregunta, tenes '+chances+' chance. ¡Vamos al sorteo!');
 }
 
+imprimirLog ('Chances: '+chances);
+imprimirLog ('Numero Ganador: '+numGanador);
+
+while (chances != 0) {
+    let numElegido = parseInt(prompt ("Adivina el número del 1 al 10, te quedan " + (chances) + " intentos"));
+    if (numGanador==numElegido) {
+        alert('¡¡¡Ganaste!!! Tenemos un mes de prueba gratis de Tu Gestión FULL. Enviaremos todo al correo que nos proporcionaste.');
+        chances--
+        ganador = true;
+        break;
+    } else if (numElegido > 10){
+        alert('Elegí un numero del 1 al 10.');
+    } else if (chances == 1){
+        alert('Lo lamentamos, el número ganador era el '+ numGanador +'. Te quedaste sin chances, pero no te preocupes! Recomendanos a un compañero de trabajo, para que pueda participar con su correo y ambos prueben Tu Gestión.')
+        chances--
+    } else if (numGanador > numElegido){
+        alert('Chico, chiquito, pequeño, tu numero no es el correcto.');
+        chances--;
+    } else if (numGanador < numElegido || numElegido <= 10){
+        alert('Grande, grandote, tu numero es enorme.');
+        chances--;
+    }
+}
+
+imprimirLog ('Ganador : '+ganador)
+imprimirLog ('Chances restantes: '+chances)
 
